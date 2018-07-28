bdae3bdb2df5c93e7ae1f5b862558e4d9aa5083f
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-07-29 01:04:31 +0200

    add docs

--- src/css/css.rs
+++ src/css/css.rs
@@ -157,6 +157,23 @@ impl fmt::Display for AtRule {
     }
 }*/
 
+
+/// Minifies a given CSS source code.
+///
+/// # Example
+///
+/// ```rust
+/// extern crate minifier;
+/// use minifier::css::minify;
+///
+/// fn main() {
+///     let css = r#"
+///         .foo > p {
+///             color: red;
+///         }"#.into();
+///     let css_minified = minify(css);
+/// }
+/// ```
 pub fn minify<'a>(content: &'a str) -> Result<String, &'static str> {
     token::tokenize(content).map(|t| format!("{}", t))
 }
